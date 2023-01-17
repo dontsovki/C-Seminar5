fec3419870ae2918131a0b3ff3c0872dc47f1dfe
Artem.don <dontsovki@gmail.com>
2023-01-17 16:12:00 +0300

    Программа, которая определяет, присутствует ли заданное число в массиве.

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,39 +1,52 @@
-﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+﻿// Программа, которая определяет, присутствует ли заданное число в массиве.
 
-Clear();
-Write("Введите элементы массива через пробел: ");
-string elements = ReadLine();
-int[] baseArray = GetArrayFromString(elements);
-Write("Введите число: ");
-int n = int.Parse(ReadLine());
-if (FindElement(baseArray, n))
-{
-    WriteLine("Да");
+int[] CreateRandomArray(int N, int start, int end){
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
 }
+int[] CreateArray(){
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void ShowArray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+bool checkNum (int [] array, int num) {
+    bool finded = false;
+
+    for (int i=0; i < array.Length; i++)
+        if (array[i] == num)
+            finded = true;
+
+    return finded;
+}
+
+int [] Array = CreateArray();
+ShowArray(Array);
+
+Console.Write("Введите искомое число: ");
+string stringNum = Console.ReadLine();
+int intNum = Convert.ToInt32(stringNum);
+
+bool finded = checkNum(Array, intNum);
+
+if (finded == true) 
+    Console.Write("Искомое число существует");
 else
-{
-    WriteLine("Нет");
-}
-
-
-int[] GetArrayFromString(string stringArray)
-{
-    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-    int[] res = new int[nums.Length];
-
-    for (int i = 0; i < nums.Length; i++)
-    {
-        res[i] = int.Parse(nums[i]);
-    }
-    return res;
-}
-
-bool FindElement(int[] array, int el)
-{
-    foreach (var item in array)
-    {
-        if (el == item) return true;
-    }
-    return false;
-}
-
+    Console.Write("Искомое число не существует");
